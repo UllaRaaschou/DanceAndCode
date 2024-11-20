1ecfa54422875fe45c320b5cb62da2e52e607306
UllaRaaschou <126786969+UllaRaaschou@users.noreply.github.com>
2024-11-20 10:21:54 +0100

    first shot registrer woring hours

--- SimoneBlazor/Components/Pages/RegisterWorkingHours.razor.cs
+++ SimoneBlazor/Components/Pages/RegisterWorkingHours.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Components;
 using SimoneBlazor.Domain;
+using System.Runtime.CompilerServices;
 
 namespace SimoneBlazor.Components.Pages
 {
@@ -8,23 +9,36 @@ namespace SimoneBlazor.Components.Pages
         public TeamBlazor Team { get; set; } = new TeamBlazor();
         public bool IsVikar { get; set; } = false;
 
-
         private DateTime selectedDate = DateTime.Today;
         public string[] labelTexts = new string[] { "Løn 1", "Løn 2", "Løn 3", "Løn 4" };
-        public string[] dropdownValues = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
+        public string[] dropdownValues;
         public string[] selectedValues = new string[] { "0", "0", "0", "0" };
         private List<(DateTime Date, decimal[] Values)> savedValues = new List<(DateTime, decimal[])>();
 
+        protected override void OnInitialized()
+        {
+            dropdownValues = GetDropDownValues();
+        }
 
         public void ChangeVikarStatus()
-        {
+        {            
             IsVikar = !IsVikar;
             StateHasChanged();
         }
 
         private void SaveValues()
         {
-           savedValues.Add((selectedDate, (decimal[])selectedValues.Clone()));
+            savedValues.Add((selectedDate, (decimal[])selectedValues.Clone()));
+        }
+
+        public string[] GetDropDownValues()
+        {
+            var calculatedValues = new List<string>();
+            for (decimal i = 0.25m; i <= 5; i += 0.25m)
+            {
+                calculatedValues.Add(i.ToString());
+            }
+            return calculatedValues.ToArray();
         }
     }
 }
